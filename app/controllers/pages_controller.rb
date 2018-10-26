b0b892e4c7a58881158697987b9ae860166b037f
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-27 01:28:56 +0900

    add: ホーム画面にログアウトのリンクを追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    # もしユーザーがsign_inしていなければ
+    unless user_signed_in?
+      # 新規登録ページにリダイレクト
+      redirect_to new_user_session_path
+    end
   end
 end
